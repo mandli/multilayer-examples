1943f51f789055a3ce50528d0d9df8c6cb9815e6
Kyle Mandli <kyle.mandli@gmail.com>
2016-01-26 20:04:44 -0500

    Update well balancing tests

--- 1d/well_balanced.py
+++ 1d/well_balanced.py
@@ -22,8 +22,9 @@ def smooth_test(eigen_method, dry=False, **kargs):
     
     # Redirect loggers
     # This is not working for all cases, see comments in runclaw.py
-    for logger_name in ['pyclaw.io','pyclaw.solution','plot','pyclaw.solver','f2py','data']:
-        runclaw.replace_stream_handlers(logger_name,log_path,log_file_append=False)
+    for logger_name in ['pyclaw.io', 'pyclaw.solution', 'plot', 'pyclaw.solver',
+                        'f2py','data']:
+        runclaw.replace_stream_handlers(logger_name, log_path, log_file_append=False)
 
     # Load in appropriate PyClaw version
     if kargs.get('use_petsc',False):
@@ -35,7 +36,7 @@ def smooth_test(eigen_method, dry=False, **kargs):
     # = Create Solver =
     # =================
     if kargs.get('solver_type','classic') == 'classic':
-        solver = pyclaw.ClawSolver1D()
+        solver = pyclaw.ClawSolver1D(riemann_solver=layered_shallow_water_1D)
     else:
         raise NotImplementedError('Classic is currently the only supported solver.')
         
@@ -45,7 +46,6 @@ def smooth_test(eigen_method, dry=False, **kargs):
     solver.max_steps = 5000
     solver.fwave = True
     solver.kernel_language = 'Fortran'
-    solver.num_waves = 4
     solver.limiters = 3
     solver.source_split = 1
         
@@ -55,9 +55,6 @@ def smooth_test(eigen_method, dry=False, **kargs):
     solver.bc_upper[0] = 1
     solver.aux_bc_lower[0] = 1
     solver.aux_bc_upper[0] = 1
-    
-    # Set the Riemann solver
-    solver.rp = layered_shallow_water_1D
 
     # Set the before step function
     solver.before_step = lambda solver,solution:ml.step.before_step(solver,
@@ -72,9 +69,9 @@ def smooth_test(eigen_method, dry=False, **kargs):
     # ============================
     num_layers = 2
     
-    x = pyclaw.Dimension('x',0.0,10.0,200)
+    x = pyclaw.Dimension(0.0, 10.0, 200)
     domain = pyclaw.Domain([x])
-    state = pyclaw.State(domain,2*num_layers,3+num_layers)
+    state = pyclaw.State(domain, 2 * num_layers, 3 + num_layers)
     state.aux[ml.aux.kappa_index,:] = 0.0
 
     # Set physics data
@@ -96,12 +93,13 @@ def smooth_test(eigen_method, dry=False, **kargs):
     solution.t = 0.0
     
     # Set aux arrays including bathymetry, wind field and linearized depths
-    ml.aux.set_gaussian_bathymetry(solution.state,10.0,5,numpy.sqrt(5/2),5.0)
+    ml.aux.set_gaussian_bathymetry(solution.state, 10.0, 5, numpy.sqrt(5 / 2), 
+                                                   5.0)
     ml.aux.set_no_wind(solution.state)
     if dry:
-        ml.aux.set_h_hat(solution.state,0.5,[0.0,-6.0],[0.0,-6.0])
+        ml.aux.set_h_hat(solution.state, 0.5, [0.0, -6.0], [0.0, -6.0])
     else:
-        ml.aux.set_h_hat(solution.state,0.5,[0.0,-4.0],[0.0,-4.0])
+        ml.aux.set_h_hat(solution.state, 0.5, [0.0, -4.0], [0.0, -4.0])
 
     # Set perturbation to sea at rest
     ml.qinit.set_quiescent_init_condition(solution.state)
@@ -148,7 +146,8 @@ def jump_test(eigen_method, dry=False, **kargs):
     
     # Redirect loggers
     # This is not working for all cases, see comments in runclaw.py
-    for logger_name in ['io','solution','plot','evolve','f2py','data']:
+    for logger_name in ['pyclaw.io', 'pyclaw.solution', 'plot', 'pyclaw.solver',
+                        'f2py','data']:
         runclaw.replace_stream_handlers(logger_name,log_path,log_file_append=False)
 
     # Load in appropriate PyClaw version
@@ -161,7 +160,7 @@ def jump_test(eigen_method, dry=False, **kargs):
     # = Create Solver =
     # =================
     if kargs.get('solver_type','classic') == 'classic':
-        solver = pyclaw.ClawSolver1D()
+        solver = pyclaw.ClawSolver1D(riemann_solver=layered_shallow_water_1D)
     else:
         raise NotImplementedError('Classic is currently the only supported solver.')
         
@@ -171,7 +170,6 @@ def jump_test(eigen_method, dry=False, **kargs):
     solver.max_steps = 5000
     solver.fwave = True
     solver.kernel_language = 'Fortran'
-    solver.num_waves = 4
     solver.limiters = 3
     solver.source_split = 1
         
@@ -181,13 +179,10 @@ def jump_test(eigen_method, dry=False, **kargs):
     solver.bc_upper[0] = 1
     solver.aux_bc_lower[0] = 1
     solver.aux_bc_upper[0] = 1
-    
-    # Set the Riemann solver
-    solver.rp = riemann.layered_shallow_water_1D
 
     # Set the before step function
-    solver.before_step = lambda solver,solution:ml.step.before_step(solver,
-                                                                    solution)
+    solver.before_step = lambda solver, solution:ml.step.before_step(solver,
+                                                                     solution)
                                             
     # Use simple friction source term
     solver.step_source = ml.step.friction_source
@@ -198,9 +193,9 @@ def jump_test(eigen_method, dry=False, **kargs):
     # ============================
     num_layers = 2
     
-    x = pyclaw.Dimension('x',0.0,10.0,200)
+    x = pyclaw.Dimension(0.0,10.0,200)
     domain = pyclaw.Domain([x])
-    state = pyclaw.State(domain,2*num_layers,3+num_layers)
+    state = pyclaw.State(domain, 2 * num_layers, 3 + num_layers)
     state.aux[ml.aux.kappa_index,:] = 0.0
 
     # Set physics data
@@ -222,12 +217,12 @@ def jump_test(eigen_method, dry=False, **kargs):
     solution.t = 0.0
     
     # Set aux arrays including bathymetry, wind field and linearized depths
-    ml.aux.set_jump_bathymetry(solution.state,5.0,[-10.0,-5.0])
+    ml.aux.set_jump_bathymetry(solution.state, 5.0, [-10.0, -5.0])
     ml.aux.set_no_wind(solution.state)
     if dry:
-        ml.aux.set_h_hat(solution.state,0.5,[0.0,-6.0],[0.0,-6.0])
+        ml.aux.set_h_hat(solution.state, 0.5, [0.0, -6.0], [0.0, -6.0])
     else:
-        ml.aux.set_h_hat(solution.state,0.5,[0.0,-4.0],[0.0,-4.0])
+        ml.aux.set_h_hat(solution.state, 0.5, [0.0, -4.0], [0.0, -4.0])
 
     # Set perturbation to sea at rest
     ml.qinit.set_quiescent_init_condition(solution.state)
