fd0f8f447a2562c8bd2aaf2b5d9ebc624cb92507
Kyle Mandli <kyle.mandli@gmail.com>
2016-01-26 19:46:30 -0500

    Minor changes to match auto RP

--- 1d/dry_state.py
+++ 1d/dry_state.py
@@ -3,8 +3,6 @@
 
 r""" Run the suite of tests for the 1d two-layer equations"""
 
-import sys
-
 from clawpack.riemann import layered_shallow_water_1D
 import clawpack.clawutil.runclaw as runclaw
 import clawpack.pyclaw.plot as plot
@@ -16,17 +14,18 @@ def dry_state(num_cells,eigen_method,entropy_fix,**kargs):
     
     # Construct output and plot directory paths
     name = 'multilayer/dry_state'
-    prefix = 'ml_e%s_m%s_fix' % (eigen_method,num_cells)
+    prefix = 'ml_e%s_m%s_fix' % (eigen_method, num_cells)
     
     if entropy_fix:
-        prefix = "".join((prefix,"T"))
+        prefix = "".join((prefix, "T"))
     else:
-        prefix = "".join((prefix,"F"))
-    outdir,plotdir,log_path = runclaw.create_output_paths(name,prefix,**kargs)
+        prefix = "".join((prefix, "F"))
+    outdir,plotdir,log_path = runclaw.create_output_paths(name, prefix, **kargs)
     
     # Redirect loggers
     # This is not working for all cases, see comments in runclaw.py
-    for logger_name in ['pyclaw.io','pyclaw.solution','plot','pyclaw.solver','f2py','data']:
+    for logger_name in ['pyclaw.io', 'pyclaw.solution', 'plot', 'pyclaw.solver',
+                        'f2py','data']:
         runclaw.replace_stream_handlers(logger_name,log_path,log_file_append=False)
 
     # Load in appropriate PyClaw version
@@ -39,8 +38,8 @@ def dry_state(num_cells,eigen_method,entropy_fix,**kargs):
     # =================
     # = Create Solver =
     # =================
-    if kargs.get('solver_type','classic') == 'classic':
-        solver = pyclaw.ClawSolver1D()
+    if kargs.get('solver_type', 'classic') == 'classic':
+        solver = pyclaw.ClawSolver1D(riemann_solver=layered_shallow_water_1D)
     else:
         raise NotImplementedError('Classic is currently the only supported solver.')
         
@@ -50,7 +49,6 @@ def dry_state(num_cells,eigen_method,entropy_fix,**kargs):
     solver.max_steps = 5000
     solver.fwave = True
     solver.kernel_language = 'Fortran'
-    solver.num_waves = 4
     solver.limiters = 3
     solver.source_split = 1
         
@@ -59,12 +57,10 @@ def dry_state(num_cells,eigen_method,entropy_fix,**kargs):
     solver.bc_upper[0] = 1
     solver.aux_bc_lower[0] = 1
     solver.aux_bc_upper[0] = 1
-    
-    # Set the Riemann solver
-    solver.rp = layered_shallow_water_1D
 
     # Set the before step function
-    solver.before_step = lambda solver,solution:ml.step.before_step(solver,solution)
+    solver.before_step = lambda solver, solution:ml.step.before_step(
+                                                               solver, solution)
                                             
     # Use simple friction source term
     solver.step_source = ml.step.friction_source
@@ -74,17 +70,18 @@ def dry_state(num_cells,eigen_method,entropy_fix,**kargs):
     # ============================
     num_layers = 2
     
-    x = pyclaw.Dimension('x',0.0,1.0,num_cells)
+    x = pyclaw.Dimension(0.0, 1.0, num_cells)
     domain = pyclaw.Domain([x])
-    state = pyclaw.State(domain,2*num_layers,3+num_layers)
+    state = pyclaw.State(domain, 2 * num_layers, 3 + num_layers)
     state.aux[ml.aux.kappa_index,:] = 0.0
 
     # Set physics data
     state.problem_data['g'] = 9.8
     state.problem_data['manning'] = 0.0
     state.problem_data['rho_air'] = 1.15e-3
-    state.problem_data['rho'] = [0.95,1.0]
-    state.problem_data['r'] = state.problem_data['rho'][0] / state.problem_data['rho'][1]
+    state.problem_data['rho'] = [0.95, 1.0]
+    state.problem_data['r'] =   \
+                     state.problem_data['rho'][0] / state.problem_data['rho'][1]
     state.problem_data['one_minus_r'] = 1.0 - state.problem_data['r']
     state.problem_data['num_layers'] = num_layers
     
@@ -94,20 +91,20 @@ def dry_state(num_cells,eigen_method,entropy_fix,**kargs):
     state.problem_data['inundation_method'] = 2
     state.problem_data['entropy_fix'] = entropy_fix
     
-    solution = pyclaw.Solution(state,domain)
+    solution = pyclaw.Solution(state, domain)
     solution.t = 0.0
     
     # Set aux arrays including bathymetry, wind field and linearized depths
-    ml.aux.set_jump_bathymetry(solution.state,0.5,[-1.0,-1.0])
+    ml.aux.set_jump_bathymetry(solution.state, 0.5, [-1.0, -1.0])
     ml.aux.set_no_wind(solution.state)
-    ml.aux.set_h_hat(solution.state,0.5,[0.0,-0.5],[0.0,-1.0])
+    ml.aux.set_h_hat(solution.state, 0.5, [0.0,-0.5], [0.0,-1.0])
     
     # Set sea at rest initial condition
-    q_left = [0.5 * state.problem_data['rho'][0],0.0,
-              0.5 * state.problem_data['rho'][1],0.0]
-    q_right = [1.0 * state.problem_data['rho'][0],0.0,
-               0.0,0.0]
-    ml.qinit.set_riemann_init_condition(solution.state,0.5,q_left,q_right)
+    q_left = [0.5 * state.problem_data['rho'][0], 0.0,
+              0.5 * state.problem_data['rho'][1], 0.0]
+    q_right = [1.0 * state.problem_data['rho'][0], 0.0,
+               0.0, 0.0]
+    ml.qinit.set_riemann_init_condition(solution.state, 0.5, q_left, q_right)
     
     
     # ================================
