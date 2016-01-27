81a3788149975048343387ff544ad7b930728582
Kyle Mandli <kyle.mandli@gmail.com>
2016-01-26 19:50:36 -0500

    update oscillatory wind example

--- 1d/oscillatory.py
+++ 1d/oscillatory.py
@@ -15,12 +15,14 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
     # Construct output and plot directory paths
     prefix = 'ml_e%s_n%s' % (eigen_method,num_cells)
     name = 'multilayer/oscillatory_wind'
-    outdir,plotdir,log_path = runclaw.create_output_paths(name,prefix,**kargs)
+    outdir,plotdir,log_path = runclaw.create_output_paths(name, prefix, **kargs)
     
     # Redirect loggers
     # This is not working for all cases, see comments in runclaw.py
-    for logger_name in ['pyclaw.io','pyclaw.solution','plot','pyclaw.solver','f2py','data']:
-        runclaw.replace_stream_handlers(logger_name,log_path,log_file_append=False)
+    for logger_name in ['pyclaw.io', 'pyclaw.solution', 'plot', 'pyclaw.solver',
+                       'f2py', 'data']:
+        runclaw.replace_stream_handlers(logger_name, log_path, 
+                                            log_file_append=False)
 
     # Load in appropriate PyClaw version
     if kargs.get('use_petsc',False):
@@ -32,7 +34,7 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
     # = Create Solver =
     # =================
     if kargs.get('solver_type','classic') == 'classic':
-        solver = pyclaw.ClawSolver1D()
+        solver = pyclaw.ClawSolver1D(riemann_solver=layered_shallow_water_1D)
     else:
         raise NotImplementedError('Classic is currently the only supported solver.')
         
@@ -42,7 +44,6 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
     solver.max_steps = 5000
     solver.fwave = True
     solver.kernel_language = 'Fortran'
-    solver.num_waves = 4
     solver.limiters = 3
     solver.source_split = 1
         
@@ -55,15 +56,13 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
     solver.user_bc_upper = ml.bc.wall_qbc_upper
     solver.aux_bc_lower[0] = 1
     solver.aux_bc_upper[0] = 1
-    
-    # Set the Riemann solver
-    solver.rp = layered_shallow_water_1D
 
     # Set the before step functioning including the wind forcing
     wind_func = lambda state:ml.aux.set_oscillatory_wind(state,
-                                        A=5.0,N=2.0,omega=2.0,t_length=10.0)
-    solver.before_step = lambda solver,solution:ml.step.before_step(solver,solution,
-                                            wind_func=wind_func,raise_on_richardson=True)
+                                        A=5.0, N=2.0, omega=2.0, t_length=10.0)
+    solver.before_step = lambda solver, solution:ml.step.before_step(solver, 
+                                            solution, wind_func=wind_func, 
+                                            raise_on_richardson=True)
                                             
     # Use simple friction source term
     solver.step_source = ml.step.friction_source
@@ -73,17 +72,18 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
     # ============================
     num_layers = 2
     
-    x = pyclaw.Dimension('x',0.0,1.0,num_cells)
+    x = pyclaw.Dimension(0.0, 1.0, num_cells)
     domain = pyclaw.Domain([x])
-    state = pyclaw.State(domain,2*num_layers,3+num_layers)
+    state = pyclaw.State(domain, 2 * num_layers, 3 + num_layers)
     state.aux[ml.aux.kappa_index,:] = 0.0
 
     # Set physics data
     state.problem_data['g'] = 9.8
     state.problem_data['manning'] = 0.0
     state.problem_data['rho_air'] = 1.15
-    state.problem_data['rho'] = [1025.0,1045.0]
-    state.problem_data['r'] = state.problem_data['rho'][0] / state.problem_data['rho'][1]
+    state.problem_data['rho'] = [1025.0, 1045.0]
+    state.problem_data['r'] = \
+                     state.problem_data['rho'][0] / state.problem_data['rho'][1]
     state.problem_data['one_minus_r'] = 1.0 - state.problem_data['r']
     state.problem_data['num_layers'] = num_layers
     
@@ -97,9 +97,9 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
     solution.t = 0.0
     
     # Set aux arrays including bathymetry, wind field and linearized depths
-    ml.aux.set_jump_bathymetry(solution.state,0.5,[-1.0,-1.0])
+    ml.aux.set_jump_bathymetry(solution.state, 0.5, [-1.0, -1.0])
     wind_func(solution.state)
-    ml.aux.set_h_hat(solution.state,0.5,[0.0,-0.25],[0.0,-0.25])
+    ml.aux.set_h_hat(solution.state, 0.5, [0.0, -0.25], [0.0, -0.25])
     
     # Set sea at rest initial condition
     ml.qinit.set_quiescent_init_condition(solution.state)
@@ -137,11 +137,11 @@ def oscillatory_wind(num_cells,eigen_method,**kargs):
                   'xupper':solution.state.grid.x.upper,
                   'rho':solution.state.problem_data['rho'],
                   'dry_tolerance':solution.state.problem_data['dry_tolerance']}
-    plot(setplot="./setplot_oscillatory.py",outdir=outdir,
-         plotdir=plotdir,htmlplot=kargs.get('htmlplot',False),
-         iplot=kargs.get('iplot',False),file_format=controller.output_format,
+    plot(setplot="./setplot_oscillatory.py", outdir=outdir,
+         plotdir=plotdir, htmlplot=kargs.get('htmlplot',False),
+         iplot=kargs.get('iplot',False), file_format=controller.output_format,
          **plot_kargs)
          
          
 if __name__ == "__main__":
-    oscillatory_wind(100,2,htmlplot=True)
+    oscillatory_wind(100, 2, htmlplot=True)
