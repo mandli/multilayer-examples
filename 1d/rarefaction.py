046c3761a5b0c4acbc8382151d32ce4db2b23b75
Kyle Mandli <kyle.mandli@gmail.com>
2016-01-26 19:52:49 -0500

    Update rarefaction example

--- 1d/rarefaction.py
+++ 1d/rarefaction.py
@@ -15,19 +15,21 @@ def rarefaction(num_cells,eigen_method,entropy_fix,**kargs):
     # Construct output and plot directory paths
     prefix = 'ml_e%s_m%s_fix' % (eigen_method,num_cells)
     if entropy_fix:
-        prefix = "".join((prefix,"T"))
+        prefix = "".join((prefix, "T"))
     else:
-        prefix = "".join((prefix,"F"))
+        prefix = "".join((prefix, "F"))
     name = 'multilayer/all_rare'
-    outdir,plotdir,log_path = runclaw.create_output_paths(name,prefix,**kargs)
+    outdir, plotdir, log_path = runclaw.create_output_paths(name, prefix, **kargs)
     
     # Redirect loggers
     # This is not working for all cases, see comments in runclaw.py
-    for logger_name in ['pyclaw.io','pyclaw.solution','plot','pyclaw.solver','f2py','data']:
-        runclaw.replace_stream_handlers(logger_name,log_path,log_file_append=False)
+    for logger_name in ['pyclaw.io', 'pyclaw.solution', 'plot', 'pyclaw.solver',
+                        'f2py', 'data']:
+        runclaw.replace_stream_handlers(logger_name, log_path,
+                                            log_file_append=False)
 
     # Load in appropriate PyClaw version
-    if kargs.get('use_petsc',False):
+    if kargs.get('use_petsc', False):
         import clawpack.petclaw as pyclaw
     else:
         import clawpack.pyclaw as pyclaw
@@ -37,7 +39,7 @@ def rarefaction(num_cells,eigen_method,entropy_fix,**kargs):
     # = Create Solver =
     # =================
     if kargs.get('solver_type','classic') == 'classic':
-        solver = pyclaw.ClawSolver1D()
+        solver = pyclaw.ClawSolver1D(riemann_solver=layered_shallow_water_1D)
     else:
         raise NotImplementedError('Classic is currently the only supported solver.')
         
@@ -47,7 +49,6 @@ def rarefaction(num_cells,eigen_method,entropy_fix,**kargs):
     solver.max_steps = 5000
     solver.fwave = True
     solver.kernel_language = 'Fortran'
-    solver.num_waves = 4
     solver.limiters = 3
     solver.source_split = 1
         
@@ -56,12 +57,10 @@ def rarefaction(num_cells,eigen_method,entropy_fix,**kargs):
     solver.bc_upper[0] = 1
     solver.aux_bc_lower[0] = 1
     solver.aux_bc_upper[0] = 1
-    
-    # Set the Riemann solver
-    solver.rp = layered_shallow_water_1D
 
     # Set the before step function
-    solver.before_step = lambda solver,solution:ml.step.before_step(solver,solution)
+    solver.before_step = lambda solver,solution:ml.step.before_step(solver, 
+                                                                    solution)
                                             
     # Use simple friction source term
     solver.step_source = ml.step.friction_source
@@ -72,9 +71,9 @@ def rarefaction(num_cells,eigen_method,entropy_fix,**kargs):
     # ============================
     num_layers = 2
     
-    x = pyclaw.Dimension('x',0.0,1.0,num_cells)
+    x = pyclaw.Dimension(0.0, 1.0, num_cells)
     domain = pyclaw.Domain([x])
-    state = pyclaw.State(domain,2*num_layers,3+num_layers)
+    state = pyclaw.State(domain, 2 * num_layers, 3 + num_layers)
     state.aux[ml.aux.kappa_index,:] = 0.0
 
     # Set physics data
@@ -96,10 +95,10 @@ def rarefaction(num_cells,eigen_method,entropy_fix,**kargs):
     solution.t = 0.0
     
     # Set aux arrays including bathymetry, wind field and linearized depths
-    eta = [0.0,-0.5]
-    ml.aux.set_jump_bathymetry(solution.state,0.5,[-1.0,-1.0])
+    eta = [0.0, -0.5]
+    ml.aux.set_jump_bathymetry(solution.state, 0.5, [-1.0, -1.0])
     ml.aux.set_no_wind(solution.state)
-    ml.aux.set_h_hat(solution.state,0.5,eta,eta)
+    ml.aux.set_h_hat(solution.state, 0.5, eta, eta)
     
     # Set sea at rest initial condition with diverging velocities
     u_left = [0.0,-0.5]
